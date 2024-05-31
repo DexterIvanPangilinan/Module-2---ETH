68c7543b0c08c5513a77b06bf7da29ace276f1f6
DexterIvanPangilinan <138690462+DexterIvanPangilinan@users.noreply.github.com>
2024-05-31 11:28:45 +0800

    Update Assessment.sol

--- contracts/Assessment.sol
+++ contracts/Assessment.sol
@@ -1,62 +1,45 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.9;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
-contract Deksyy {
-    address payable public owner;
-    uint256 public balance;
-    string public charName;
+contract Assessment {
+    mapping(address => uint256) private balances;
 
-    event Deposit(uint256 amount);
-    event Withdraw(uint256 amount);
-    event CharNameAssigned(string name);
-
-    constructor(uint initBalance) payable {
-        owner = payable(msg.sender);
-        balance = initBalance;
-    }
+    event Deposit(address indexed user, uint256 amount);
+    event Withdraw(address indexed user, uint256 amount);
 
     function getBalance() public view returns (uint256) {
-        return balance;
+        return balances[msg.sender];
     }
 
-    function deposit(uint256 _amount) public payable {
-        uint _previousBalance = balance;
-
-        require(msg.sender == owner, "You are not the owner of this account");
-
-        balance += _amount;
-
-        assert(balance == _previousBalance + _amount);
-
-        emit Deposit(_amount);
+    function deposit(uint256 amount) public {
+        require(amount > 0, "Deposit amount must be greater than zero");
+        balances[msg.sender] += amount;
+        emit Deposit(msg.sender, amount);
     }
 
-    error InsufficientBalance(uint256 balance, uint256 withdrawAmount);
-
-    function withdraw(uint256 _withdrawAmount) public {
-        require(msg.sender == owner, "You are not the owner of this account");
-        uint _previousBalance = balance;
-        if (balance < _withdrawAmount) {
-            revert InsufficientBalance({
-                balance: balance,
-                withdrawAmount: _withdrawAmount
-            });
-        }
-
-        balance -= _withdrawAmount;
-
-        assert(balance == (_previousBalance - _withdrawAmount));
-
-        emit Withdraw(_withdrawAmount);
+    function withdraw(uint256 amount) public {
+        require(amount <= balances[msg.sender], "Insufficient balance");
+        require(amount > 0, "Withdraw amount must be greater than zero");
+        balances[msg.sender] -= amount;
+        emit Withdraw(msg.sender, amount);
     }
 
-    function assignCharName(string memory _charName) public {
-        require(msg.sender == owner, "You are not the owner of this account");
-        charName = _charName;
-        emit CharNameAssigned(_charName);
+    function withdrawAll() public {
+        uint256 amount = balances[msg.sender];
+        require(amount > 0, "Insufficient balance");
+        balances[msg.sender] = 0;
+        emit Withdraw(msg.sender, amount);
     }
 
-    function getCharName() public view returns (string memory) {
-        return charName;
+    function randomizeTokens(uint256 amount) public {
+        require(amount > 0, "Token amount must be greater than zero");
+        balances[msg.sender] += amount;
+        emit Deposit(msg.sender, amount);
+    }
+
+    function randomizeCar(uint256 carCost) public {
+        require(carCost <= balances[msg.sender], "Insufficient balance for car");
+        balances[msg.sender] -= carCost;
+        emit Withdraw(msg.sender, carCost);
     }
 }
